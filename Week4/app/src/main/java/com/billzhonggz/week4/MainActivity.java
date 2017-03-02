884a9eb1f5fda1b0065393324d85a4456c790101
Bill ZHONG <billzhonggz@outlook.com>
2017-03-02 14:46:55 +0800

    Add listener by anonymous class for button 0.

--- Week4/app/src/main/java/com/billzhonggz/week4/MainActivity.java
+++ Week4/app/src/main/java/com/billzhonggz/week4/MainActivity.java
@@ -3,9 +3,20 @@ package com.billzhonggz.week4;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+//    // Every listener for button should implements this interface.
+//    private class MyClickListener implements View.OnClickListener{
+//
+//        @Override
+//        public void onClick(View view) {
+//            Toast.makeText(view.getContext(),"Button 0 has been clicked!",Toast.LENGTH_SHORT).show();
+//        }
+//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +24,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"This is a test message.",Toast.LENGTH_SHORT).show();
         Log.d("onCreate","a message");
+        Button b = (Button)findViewById(R.id.button11);
+//        MyClickListener l = new MyClickListener();
+        b.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(),"Button 0 has been clicked!",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
