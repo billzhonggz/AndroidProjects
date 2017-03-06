a68f54a78c7b34a028a51ffc233abb6abef3c9e0
Bill ZHONG <billzhonggz@outlook.com>
2017-03-06 13:59:47 +0800

    Add question 1-4 of week 5 lab.

---
+++ Week5/app/src/main/java/com/billzhonggz/week5/MainActivity.java
@@ -0,0 +1,62 @@
+package com.billzhonggz.week5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // Initialize changing variable.
+    private int i = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Set up text view.
+        final TextView t = (TextView)findViewById(R.id.textView);
+        t.setText(Integer.toString(i));
+
+        // Find out the buttons for increment in xml file.
+        Button add = (Button)findViewById(R.id.addBtn);
+        Button minor = (Button)findViewById(R.id.minorBtn);
+        Button reset = (Button)findViewById(R.id.resetBtn);
+        // Do add.
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Increment i and update.
+                i++;
+                String display = Integer.toString(i);
+                t.setText(display);
+            }
+        });
+
+        // Do minor.
+        minor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Decrease i.
+                i--;
+                String display = Integer.toString(i);
+                t.setText(display);
+            }
+        });
+
+        // Do reset.
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Reset i.
+                i = 0;
+                String display = Integer.toString(i);
+                t.setText(display);
+            }
+        });
+
+        // Checkbox and toasts.
+        // Find
+    }
+}
