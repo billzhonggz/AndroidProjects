15752b5517663aee4b9c0c3ca7fdc0998110cdf1
Bill ZHONG <billzhonggz@outlook.com>
2017-03-09 14:18:36 +0800

    Add a implicit intent with opening a webpage.

--- Week5/app/src/main/java/com/billzhonggz/week5/SecondActivity.java
+++ Week5/app/src/main/java/com/billzhonggz/week5/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.billzhonggz.week5;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button web = (Button)findViewById(R.id.webBtn);
+        web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("http://uic.edu.hk/"));
+                startActivity(i);
+            }
+        });
     }
 }
