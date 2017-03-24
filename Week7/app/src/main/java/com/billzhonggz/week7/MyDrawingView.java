fb0d737d4e6ad9b7805741f8946462b8cc91154d
Bill ZHONG <billzhonggz@outlook.com>
2017-03-24 21:46:22 +0800

    Update Week 7.

--- Week7/app/src/main/java/com/billzhonggz/week7/MyDrawingView.java
+++ Week7/app/src/main/java/com/billzhonggz/week7/MyDrawingView.java
@@ -5,10 +5,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PointF;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Toast;
+import android.os.Handler;
 
 import java.util.ArrayList;
 
@@ -20,6 +22,9 @@ public class MyDrawingView extends View {
 //    private Paint p2 = new Paint();
     private Paint p3 = new Paint();
     private ArrayList<PointF> pointList = new ArrayList<PointF>();
+    private ArrayList<PointF> undoList = new ArrayList<PointF>();
+    private long delay = 3000;
+    private Handler h;
 
     public MyDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -29,22 +34,56 @@ public class MyDrawingView extends View {
         // Draw cylinder.
 //        p2.setColor(Color.BLUE);
 //        p2.setStyle(Paint.Style.STROKE);
-        p3.setColor(Color.RED);
-        p3.setStyle(Paint.Style.STROKE);
-        this.setOnTouchListener(new OnTouchListener() {
+//        p3.setColor(Color.RED);
+//        p3.setStyle(Paint.Style.STROKE);
+//        this.setOnTouchListener(new OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View view, MotionEvent motionEvent) {
+//                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+//                    // Get touch position.
+//                    PointF point = new PointF();
+//                    point.set(motionEvent.getX(), motionEvent.getY());
+//                    pointList.add(point);
+//                    //Toast.makeText(view.getContext(), "x=" + x + "y=" + y, Toast.LENGTH_SHORT).show();
+//                    invalidate();
+//                }
+//                return true;
+//            }
+//        });
+        // Test multiple listeners.
+//        this.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(view.getContext(),"OnClick",Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//        this.setOnLongClickListener(new OnLongClickListener() {
+//            @Override
+//            public boolean onLongClick(View view) {
+//                Toast.makeText(view.getContext(),"OnLongClick",Toast.LENGTH_SHORT).show();
+//                return true;
+//            }
+//        });
+//        this.setOnTouchListener(new OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View view, MotionEvent motionEvent) {
+//                Toast.makeText(view.getContext(),"OnTouch" + motionEvent, Toast.LENGTH_SHORT).show();
+//                return false;
+//            }
+//        });
+        // Test message queue.
+        h = new Handler(new Handler.Callback() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    // Get touch position.
-                    PointF point = new PointF();
-                    point.set(motionEvent.getX(), motionEvent.getY());
-                    pointList.add(point);
-                    //Toast.makeText(view.getContext(), "x=" + x + "y=" + y, Toast.LENGTH_SHORT).show();
-                    invalidate();
-                }
-                return true;
+            public boolean handleMessage(Message message) {
+                MyDrawingView mdv = (MyDrawingView) findViewById(R.id.drawing_area);
+                Toast.makeText(mdv.getContext(), "tick", Toast.LENGTH_SHORT).show();
+                Message m = h.obtainMessage(0);
+                h.sendMessageDelayed(m, delay);
+                return false;
             }
         });
+        Message m = h.obtainMessage(0);
+        h.sendMessageDelayed(m, delay);
     }
 
     protected void onDraw(Canvas c) {
@@ -78,7 +117,13 @@ public class MyDrawingView extends View {
     }
 
     public void undoPoint() {
+        undoList.add(pointList.get(pointList.size() - 1));
         pointList.remove(pointList.size() - 1);
         invalidate();
+    }
+
+    public void redoPoint() {
+        pointList.add(undoList.get(undoList.size() - 1));
+        undoList.remove(undoList.size() - 1);
     }
 }
